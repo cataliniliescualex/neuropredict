b84c3470483d83eb3f56642c62b3d23e99b8f314
cataliniliescualex <71380375+cataliniliescualex@users.noreply.github.com>
2023-08-08 12:34:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,46 @@
-from flask import Flask, request, render_template
-app = Flask(__name__)
-from flask import request, render_template
-import requests
-import json
+
+from flask import Flask, render_template, request, jsonify
 import numpy as np
+from tensorflow.keras.models import load_model
+import re
+import base64
+import io
 
-@app.route('/', methods=['POST'])
+# Initialize Flask app
+app = Flask(__name__)
+
+# Load the model
+model = load_model("mnist_model.h5")
+
+def preprocess_canvas_data(data):
+    # Process the canvas data URL to obtain the image data
+    data = re.sub('^data:image/.+;base64,', '', data)
+    data = base64.b64decode(data)
+    data = io.BytesIO(data)
+    im = Image.open(data).convert("L")
+    im = im.resize((28, 28))
+    im = np.array(im)
+    im = im.reshape(1, 28, 28, 1)
+    im = im/255.0
+    return im
+
+@app.route('/', methods=['GET', 'POST'])
 def predict_digit():
-    # Get canvas data from form
-    canvas_data = request.form['canvasData']
-
-    # Preprocess canvas data into appropriate format
-    # This depends on how you implemented the drawing on the canvas
-    # But it should result in a numpy array of shape (1, 784)
-    data = preprocess_canvas_data(canvas_data)
-
-    # Convert to JSON string
-    input_data = json.dumps({'data': data.tolist()})
-
-    # Set the content type
-    headers = {'Content-Type': 'application/json'}
-
-    # Make the request and display the response
-    resp = requests.post(scoring_uri, input_data, headers=headers)
-    return render_template('index.html', prediction=resp.text)
-
-@app.route('/')
-def home():
+    if request.method == 'POST':
+        # Get canvas data from the POST request
+        canvas_data = request.form['canvas_data']
+        
+        # Preprocess the canvas data
+        data = preprocess_canvas_data(canvas_data)
+        
+        # Make prediction
+        prediction = model.predict(data)
+        predicted_class = np.argmax(prediction)
+        
+        # Return the prediction
+        return jsonify({'predicted_class': int(predicted_class)})
+    
     return render_template('index.html')
 
+if __name__ == '__main__':
+    app.run(debug=True)
